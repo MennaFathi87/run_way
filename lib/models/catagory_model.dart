5fb890dab21497650b64e78c6fec4bbd1f5d490b
MennaFathi <mnamohamed098@gmail.com>
2025-10-06 20:43:44 +0200

    add model

---
+++ lib/models/catagory_model.dart
@@ -0,0 +1,7 @@
+class CatagoryModel {
+  final String image;
+  final String title;
+
+  CatagoryModel({required this.image, required this.title});
+  
+}
---
+++ lib/models/product_model.dart
@@ -0,0 +1,7 @@
+class ProductModel {
+  final String image;
+  final String txt;
+  final String price;
+  ProductModel({required this.image,required this.price,required this.txt});
+}
+
